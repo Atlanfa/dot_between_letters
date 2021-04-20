0a992c5e74aa8d83a38d0af1d8ad52e2418bd08c
Atlanfa <iliatalgar@gmail.com>
2021-04-20 18:23:28 +0300

    fixed first task

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def dot_between(text):
     list_of_words = [[text]]
     while list_of_words[-1]:
-        list_of_words.append([word[:i] + '.' + word[i:] for word in list_of_words[-1] for i in range(1, len(word)) if word[i] != '.' and word[i-1] != '.'])
-    full_list=[]
+        list_of_words.append([word[:i] + '.' + word[i:] for word in list_of_words[-1] for i in range(1, len(word)) if
+                              word[i] != '.' and word[i - 1] != '.'])
+    full_list = []
     for lst in list_of_words:
-      full_list.extend(lst)
+        full_list.extend(lst)
     return list(dict.fromkeys(full_list))
 
 
 word = 'acbd'
 print(dot_between(word))
 print(len(dot_between(word)))
-print(2 ** (len(word)-1))
