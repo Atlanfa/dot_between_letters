c9c127795cf9bf1796c682affe6bdc0e9b87527a
Atlanfa <iliatalgar@gmail.com>
2021-04-20 12:30:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def dot_between(text):
+    list_of_words = [[text]]
+    while list_of_words[-1]:
+        list_of_words.append([word[:i] + '.' + word[i:] for word in list_of_words[-1] for i in range(1, len(word)) if word[i] != '.' and word[i-1] != '.'])
+    full_list=[]
+    for lst in list_of_words:
+      full_list.extend(lst)
+    full_list.pop(0)
+    return list(dict.fromkeys(full_list))
+
+
+word = 'abcde'
+print(dot_between(word))
+print(len(dot_between(word)))
