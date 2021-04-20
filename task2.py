03ab700f6cb5a712ca093c61b9f39d6cb9ad2f6e
Atlanfa <iliatalgar@gmail.com>
2021-04-20 14:46:47 +0300

    fixed second task

--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
 def dot_between(word):
-    list_of_dots = [bin(i)[2:] for i in range(2 ** (len(word)-1))]
+    list_of_dots = [bin(i)[2:].zfill(len(word)-1)[::-1] for i in range(2 ** (len(word)-1))]
     full_list = []
     for i in list_of_dots:
         new_word = word
         for j in range(len(str(i))):
-            if str(i[::-1])[-j] == '1':
+            if str(i)[-j] == '1':
                 new_word = word[:(len(list_of_dots[-1])-j)] + '.' + new_word[(len(list_of_dots[-1])-j):]
         full_list.append(new_word)
     return full_list
